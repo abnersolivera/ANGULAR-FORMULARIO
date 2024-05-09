7369ac73e09934a69693b843419f72047b54283e
Abner <abner.santos@bdsdatasolution.com.br>
2024-05-08 22:30:43 -0300

    menu  material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PessoaPageComponent } from './pages/pessoa-page/pessoa-page.component';
 
 const routes: Routes = [
+  {path:'pessoas', component: PessoaPageComponent},
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { HeaderComponent } from "./components/header/header.component";
+import { PessoaPageComponent } from "./pages/pessoa-page/pessoa-page.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        PessoaPageComponent
     ],
     imports:[
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatButtonModule
+        MatButtonModule,
+        MatMenuModule
     ],
     providers: [],
     bootstrap: [AppComponent]
