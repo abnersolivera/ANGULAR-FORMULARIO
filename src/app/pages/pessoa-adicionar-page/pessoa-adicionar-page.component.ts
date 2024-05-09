5d04df27fd65ba9235753b24bafb44c68ad95d7d
Abner <abner.santos@bdsdatasolution.com.br>
2024-05-08 23:13:58 -0300

    Form Material ação

--- src/app/pages/pessoa-adicionar-page/pessoa-adicionar-page.component.ts
+++ src/app/pages/pessoa-adicionar-page/pessoa-adicionar-page.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pessoa-adicionar-page',
   templateUrl: './pessoa-adicionar-page.component.html',
-  styleUrl: './pessoa-adicionar-page.component.css'
+  styleUrl: './pessoa-adicionar-page.component.css',
 })
 export class PessoaAdicionarPageComponent {
- hobbies = ['Correr', 'Nadar', 'Caminhar', 'Pedalar'];
+  pessoa: any = {};
+
+  hobbies = ['Correr', 'Nadar', 'Caminhar', 'Pedalar'];
+
+  salvar() {
+    if (this.pessoa.nome === null || this.pessoa.nome === "") {
+      alert('O campo nome é obrigatório');
+    } else {
+      console.log('Salvando pessoa.....');
+      console.log(this.pessoa);
+    }
+  }
 }
